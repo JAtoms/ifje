2605878c5ff025216a18fe7f879669ccaf136c3c
Joshua Nwokoye <joshua.nwokoye.g20@gmail.com>
2023-12-09 14:32:56 +0100

    Updating README.md file

--- compiler/Main.java
+++ compiler/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Entry point for the program
+
         String input = "add(5, mul(3, sub(10, pow(6, 4))))";
 
+        // String input = "add(pow(10, 23), mod(120, add(10, 31)))";
 
         Parser parser = new Parser(input);
-
-
         System.out.println("The expression " + input + " is equivalent to " + parser.parse());
 
     }
